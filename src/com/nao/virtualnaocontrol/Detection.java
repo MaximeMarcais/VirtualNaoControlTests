f52423d0f61352547efca77d50b22686fba2a55c
Maxime <maxime.marcais@gmail.com>
2014-11-19 17:04:30 +0200

    Début version avec nouveau T

--- src/com/nao/virtualnaocontrol/Detection.java
+++ src/com/nao/virtualnaocontrol/Detection.java
@@ -7,7 +7,12 @@ import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.calib3d.Calib3d;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfDouble;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.MatOfPoint3f;
 
 import android.app.Activity;
 import android.hardware.Sensor;
@@ -28,7 +33,7 @@ public class Detection extends Activity implements CvCameraViewListener2 {
 
 	private final static float ROBOT_HEIGHT = 580; // Exprimé en mm
 	private final static float CAMERA_FOCAL_LENGTH = 3.43f; // Exprimée en mm
-	private final static float CAMERA_HEIGHT_FROM_GROUND = 1500; // Exprimée en mm // TODO : le calculer
+	private final static float CAMERA_HEIGHT_FROM_GROUND = 1600; // Exprimée en mm // TODO : le calculer
 	private final static float ROBOT_HEIGHT_ON_SCREEN = 50;
 	private final static float DISTANCE_ROBOT_IMAGE_FROM_CENTER = 10;
 
@@ -170,6 +175,20 @@ public class Detection extends Activity implements CvCameraViewListener2 {
 		Float64Matrix extrinsicParametersMatrix = buildExtrinsicParametersMatrix((SensorManager) getSystemService(SENSOR_SERVICE));
 		Float64Matrix projectionPointCoordinates = buildProjectionPointCoordinates();
 
+		// Float64Matrix m = lu().solve(B)
+
+		// INPUT
+		// Point3 p1 = new Point3(x, y, z);
+		MatOfPoint3f objectPoints = new MatOfPoint3f(); // pts robot dans robot
+		MatOfPoint2f imagePoints = new MatOfPoint2f(); // pts robot dans camera
+		Mat cameraMatrix = new Mat(); // intrinsic
+		MatOfDouble distCoeffs = new MatOfDouble(MatOfDouble.zeros(4, 4, CvType.CV_32F)); // zeros
+
+		// OUTPUT
+		Mat rvec = new Mat();
+		Mat tvec = new Mat();
+
+		Calib3d.solvePnP(objectPoints, imagePoints, cameraMatrix, distCoeffs, rvec, tvec);
 	}
 
 	protected static Float64Matrix buildIntrinsicParametersMatrix(DisplayMetrics displayMetrics) {
